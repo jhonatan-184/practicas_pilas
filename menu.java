21edbfca2841c37baf4d6a128c4c8689f0371f04
sebasR26 <sebassrp47@gmail.com>
2026-09-10 16:43:24 -0500

    creacion metodo pagina actual y historial

--- menu.java
+++ menu.java
@@ -1,29 +1,36 @@
-
+import java.util.Scanner;
+import java.util.Stack;
 
 public class menu {
 
     public static void main(String[] args) {
-  
+        Scanner sc = new Scanner(System.in);
+        Stack<ObjpaginaWeb> pila = new Stack<>();
+        metodos m = new metodos();
         boolean continuar = true;
         while (continuar) {
             System.out.println("Menu: \n");
             System.out.println("1- Visitar pagina");
-            System.out.println("2- Retroceder");
-            System.out.println("3- Ver Historial");
-            System.out.println("4- Salir");
-            int opt = 1;
+            System.out.println("2- pagina actual");
+            System.out.println("3- Retroceder");
+            System.out.println("4- Ver Historial");
+            System.out.println("5- Salir");
+            int opt = sc.nextInt();
             switch (opt) {
                 case 1:
-                    
+                    m.visitar(pila, sc);
                     break;
                 case 2:
-                    
+                    m.actual(pila);
                     break;
                 case 3:
-                    System.out.println("mantenimiento");
-                    
+                    m.retroceder(pila);
                     break;
                 case 4:
+                    m.Historial(pila);
+                    
+                    break;
+                case 5:
                     continuar = false;
                     break;
 
--- metodos.java
+++ metodos.java
@@ -18,6 +18,11 @@ public class metodos {
             System.out.println("¿desea visitar mas paginas? si 1, no 2 ");
             int opt = sc.nextInt();
             System.out.println("============//======//=========");
+
+            o.setURL(URL);
+            o.setTitulo(Titulo);
+            o.setFechaAcceso(FechaAcceso);
+
             if (opt == 2) {
                 continuar = false;
             }
@@ -27,16 +32,35 @@ public class metodos {
 
     }
 
+    public void actual(Stack<ObjpaginaWeb> pila){
+        ObjpaginaWeb o = pila.peek();
+        System.out.println("la pagina actual es:\n");
+        System.out.println("URL: " + o.getURL());
+        System.out.println("Titulo: " + o.getTitulo());
+        System.out.println("Fecha de Aceso: " + o.getFechaAcceso());
+    }
+
     public void retroceder(Stack<ObjpaginaWeb> pila){
-        pila.pop();
-        pila.peek();
+        
         ObjpaginaWeb o = new ObjpaginaWeb();
+        o = pila.pop();
+        o=pila.peek();
+
 
         System.out.println("ahora esta en:\n");
         System.out.println("URL: " + o.getURL());
         System.out.println("Titulo: " + o.getTitulo());
         System.out.println("Fecha de Aceso: " + o.getFechaAcceso());
 
+    }
+
+     public void Historial(Stack<ObjpaginaWeb> pila) {
+        for (ObjpaginaWeb o : pila) {
+            System.out.println("URL: " + o.getURL());
+            System.out.println("Titulo: " + o.getTitulo());
+            System.out.println("Fecha de Aceso: " + o.getFechaAcceso());
+            System.out.println("------------------\n");
+        }
     }
     
 }
